326d39f27cb74e1df730c35aa0513c17c95d3f3d
Duane Haworth <duanehaworth@hotmail.com>
2020-12-27 17:14:36 -0600

    Added dialog

---
+++ client-vuetify/src/components/LoadingDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-dialog v-model="loading" persistent width="300">
+    <v-card color="primary" dark>
+      <v-card-text>
+        <div class="card-content">
+          {{msg}}
+          <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'LoadingDialog',
+  props: {
+    msg: String,
+    loading: Boolean
+  },
+}    
+</script>
+
+<style scoped lang="scss">
+  .card-content{
+      text-align: center;
+      padding: 25px 0;
+  }
+</style>
--- client-vuetify/src/views/Home.vue
+++ client-vuetify/src/views/Home.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="home">
     
-  <pre><code>{{users}}</code></pre>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+
+      <template slot="progress">
+        <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
+      </template>
+
+      <v-card-title>Users</v-card-title>
+
+    </v-card>
+  
+    
+    <template v-if="!loading">
+      <template v-if="users.length">
+        <pre><code>{{users}}</code></pre>
+        <!-- <template v-for="u in users">
+          <userSelect :user="u" :key="u.userId"></userSelect>
+        </template> -->
+      </template>    
+      <template v-else>
+        <span class="md-display-3">Currently no users</span>
+      </template>
+      <div><md-button to="/NewUser" class="md-raised md-primary">Create Account</md-button></div>
+    </template>  
+
+  <loading-dialog msg="Getting Records" v-bind:loading="loading" />
 
   </div>
 </template>
@@ -9,6 +33,7 @@
 <script>
 // @ is an alias to /src
 import statTrackerService from '@/services/statTrackerService'
+import LoadingDialog from '@/components/LoadingDialog.vue'
 
 export default {
   name: 'Home',
@@ -38,12 +63,12 @@ export default {
         }
   },    
   components: {
-    
+    LoadingDialog
   },
   created() {
-    this.getUsers();
-    // this.loading = true;
-    // setTimeout(this.getUsers, 10000);
+    //this.getUsers();
+    this.loading = true;
+    setTimeout(this.getUsers, 3000);
   }  
 }
 </script>
