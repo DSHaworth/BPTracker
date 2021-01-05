3808f6e48710fd4189bdaa6a7f11d44a30918c7e
Duane Haworth <duanehaworth@hotmail.com>
2021-01-04 18:37:54 -0600

    Moving chart components to profile

--- client-vuetify/src/views/Profile.vue
+++ client-vuetify/src/views/Profile.vue
@@ -1,58 +1,58 @@
 <template>
   <div class="about">
-    <v-card :loading="loading" class="mx-auto" style="padding: 1rem;">
+    <v-card class="mx-auto" style="padding: 1rem;">
         <h1>{{currentUser.firstname}} {{currentUser.lastname}}</h1>
 
             <div style="display: flex; flex-flow: row wrap; justify-content: flex-start; align-content: flex-start;">
                 
-                <v-card v-if="getWeightChartValues.length > 1" class="mt-4 mx-auto" width="33%" min-width="150px;">
+                <v-card v-if="userWeightStats.length > 1" class="mt-4 mx-auto" width="33%" min-width="150px;">
                     <v-sheet class="v-sheet--offset mx-auto" elevation="5" max-width="calc(100% - 32px)">
-                        <GChart type="LineChart" :data="getWeightChartValues" :options="chartOptions" :resizeDebounce="50"  />
+                        <WeightChart />
                     </v-sheet>
                     <v-card-text class="pt-0">
                     <div class="title font-weight-light mb-2">
                         User Weight
                     </div>
                     <div class="subheading font-weight-light grey--text">
-                        From {{ formatDate(getWeightChartValuesSorted[0].recordDateTime) }}
-                        To {{ formatDate(getWeightChartValuesSorted[getWeightChartValuesSorted.length - 1].recordDateTime) }}
+                        <!-- From {{ formatDate(userWeightStats[0].recordDateTime) }} -->
+                        <!-- To {{ formatDate(userWeightStats[getWeightChartValuesSorted.length - 1].recordDateTime) }} -->
                     </div>
                     <v-divider class="my-2"></v-divider>
                     <v-icon class="mr-2" small>
                         mdi-clock
                     </v-icon>
                     <span class="caption grey--text font-weight-light">
-                        {{getLastRecordedDateDays(getWeightChartValuesSorted[getWeightChartValuesSorted.length - 1])}}
+                        <!-- {{getLastRecordedDateDays(getWeightChartValuesSorted[getWeightChartValuesSorted.length - 1])}} -->
                     </span>
                     </v-card-text>
                 </v-card>
 
-                <v-card v-if="getPulseChartValues.length > 2" class="mt-4 mx-auto" width="33%">
+                <v-card v-if="userPulseStats.length > 2" class="mt-4 mx-auto" width="33%">
                     <v-sheet class="v-sheet--offset mx-auto" elevation="5" max-width="calc(100% - 32px)">                        
-                        <GChart type="LineChart" :data="getPulseChartValues" :options="chartOptions" :resizeDebounce="50"  />                        
+                        <PulseChart />
                     </v-sheet>
                     <v-card-text class="pt-0">
                     <div class="title font-weight-light mb-2">
                         User Pulse
                     </div>
                     <div class="subheading font-weight-light grey--text">
-                        From {{ formatDate(getPulseChartValuesSorted[0].recordDateTime) }}
-                        To {{ formatDate(getPulseChartValuesSorted[getPulseChartValuesSorted.length - 1].recordDateTime) }}
+                        <!-- From {{ formatDate(userPulseStats[0].recordDateTime) }} -->
+                        <!-- To {{ formatDate(userPulseStats[getPulseChartValuesSorted.length - 1].recordDateTime) }} -->
                     </div>
                     <v-divider class="my-2"></v-divider>
                     <v-icon class="mr-2" small>
                         mdi-clock
                     </v-icon>
                     <span class="caption grey--text font-weight-light">
-                        {{getLastRecordedDateDays(getPulseChartValuesSorted[getPulseChartValuesSorted.length - 1])}}
+                        <!-- {{getLastRecordedDateDays(getPulseChartValuesSorted[getPulseChartValuesSorted.length - 1])}} -->
                     </span>
                     </v-card-text>
                 </v-card>
 
-                <v-card v-if="getBpChartValuesSorted.length >= 2" class="mt-4 mx-auto" width="33%">
+                <v-card v-if="userBpStats.length >= 2" class="mt-4 mx-auto" width="33%">
                     <v-sheet class="v-sheet--offset mx-auto" elevation="5" max-width="calc(100% - 32px)">
-                        <template v-if="getBpChartValuesSorted.length >= 2">
-                            <GChart type="LineChart" :data="getBpChartValues" :options="chartOptions" :resizeDebounce="50"  />
+                        <template v-if="userBpStats.length >= 2">
+                            <BloodPressureChart />
                         </template>
                         <template v-else>
                             <div style="height: 150px; display: flex; justify-content: center; align-items: center">
@@ -65,16 +65,16 @@
                         Blood Pressure
                     </div>
                     <div class="subheading font-weight-light grey--text">
-                        From {{ formatDate(getBpChartValuesSorted[0].recordDateTime) }}
-                        To {{ formatDate(getBpChartValuesSorted[getBpChartValuesSorted.length - 1].recordDateTime) }}
+                        <!-- From {{ formatDate(userBpStats[0].recordDateTime) }} -->
+                        <!-- To {{ formatDate(userBpStats[getBpChartValuesSorted.length - 1].recordDateTime) }} -->
                     </div>
                     <v-divider class="my-2"></v-divider>
                     <v-icon class="mr-2" small>
                         mdi-clock
                     </v-icon>
                     <span class="caption grey--text font-weight-light">
-                        <template v-if="getBpChartValuesSorted.length >= 2">
-                            {{getLastRecordedDateDays(getBpChartValuesSorted[getBpChartValuesSorted.length - 1])}}
+                        <template v-if="userBpStats.length >= 2">
+                            <!-- {{getLastRecordedDateDays(getBpChartValuesSorted[getBpChartValuesSorted.length - 1])}} -->
                         </template>
                         <template v-else>
                             Not enough data
@@ -90,8 +90,11 @@
 
 <script>
 import { mapState, mapGetters  } from 'vuex'
-import { GChart } from "vue-google-charts";
+import WeightChart from "@/components/WeightChart"
+import PulseChart from "@/components/PulseChart"
+import BloodPressureChart from "@/components/BloodPressureChart"
 
+import commonService from '@/services/commonService'
 import snackbarService from '@/services/snackbarService'
 import EventBus from '@/eventBus'
 
@@ -99,72 +102,72 @@ export default {
     name: 'ProfileComponent',
     data () {
         return {
-            loading: false,
-            chartOptions: {
-                curveType: 'function',
-                legend: {position: 'none'},
-                hAxis: { 
-                    textPosition: 'none',                  
-                },
-                vAxis: { 
-                    textPosition: 'none',
-                    gridlines: {
-                        color: 'transparent'
-                    },
-                    // viewWindow: {
-                    //     min: 20,
-                    //     max: 300
-                    // },
-                    //ticks: [0, 25, 50, 75, 100] // display labels every 25                    
-                },
-                chartArea: {
-                    //left: 40,
-                    // leave room for y-axis labels
-                    width: '100%'
-                },
-                width: '100%',
-                height: '150'
-                //title: "Pulse",
-                //subtitle: "Sales, Expenses, and Profit: 2014-2017"
-            }
+            // loading: false,
+            // chartOptions: {
+            //     curveType: 'function',
+            //     legend: {position: 'none'},
+            //     hAxis: { 
+            //         textPosition: 'none',                  
+            //     },
+            //     vAxis: { 
+            //         textPosition: 'none',
+            //         gridlines: {
+            //             color: 'transparent'
+            //         },
+            //         // viewWindow: {
+            //         //     min: 20,
+            //         //     max: 300
+            //         // },
+            //         //ticks: [0, 25, 50, 75, 100] // display labels every 25                    
+            //     },
+            //     chartArea: {
+            //         //left: 40,
+            //         // leave room for y-axis labels
+            //         width: '100%'
+            //     },
+            //     width: '100%',
+            //     height: '150'
+            //     //title: "Pulse",
+            //     //subtitle: "Sales, Expenses, and Profit: 2014-2017"
+            // }
         }
     },
     computed: {
         ...mapState({currentUser: "user"}),
-        ...mapGetters(['userWeightStats', 'userPulseStats', 'userBpStats', 'isLoggedIn']),
-        getWeightChartValuesSorted: function(){
-            let sortByDate = this.userWeightStats
-                                .slice() // Make copy of original array (avoids changing original array when sorting)
-                                .sort((a, b) => Date.parse(b.recordDateTime) - Date.parse(a.recordDateTime))                                
-            return sortByDate.reverse();
-        },        
-        getWeightChartValues: function(){
-            let values = this.getWeightChartValuesSorted.map(a => [this.formatDateTime(a.recordDateTime), parseFloat(a.weight)]);
-            values.unshift(["DateTime", "Weight"]);
-            return values;
-        },
-        getPulseChartValuesSorted: function(){
-            let sortByDate = this.userPulseStats
-                                .slice() // Make copy of original array (avoids changing original array when sorting)
-                                .sort((a, b) => Date.parse(b.recordDateTime) - Date.parse(a.recordDateTime))
-            return sortByDate.reverse();
-        },        
-        getPulseChartValues: function(){
-            let values = this.getPulseChartValuesSorted.map(a => [this.formatDateTime(a.recordDateTime), parseInt(a.pulse)]);
-            values.unshift(["DateTime", "Pulse"]);
-            return values;
-        },
-        getBpChartValuesSorted: function(){
-            let sortByDate = this.userBpStats
-                                .slice() // Make copy of original array (avoids changing original array when sorting)
-                                .sort((a, b) => Date.parse(b.recordDateTime) - Date.parse(a.recordDateTime));
-            return sortByDate.reverse();
-        },           
-        getBpChartValues: function(){
-            let values = this.getBpChartValuesSorted.map( a => [this.formatDateTime(a.recordDateTime), a.sys, a.dia]);
-            values.unshift(["DateTime", "Sys", "Dia"]);
-            return values;
-        }        
+        ...mapGetters(['userWeightStats', 'userPulseStats', 'userBpStats']),
+        // getWeightChartValuesSorted: function(){
+        //     let sortByDate = this.userWeightStats
+        //                         .slice() // Make copy of original array (avoids changing original array when sorting)
+        //                         .sort((a, b) => Date.parse(b.recordDateTime) - Date.parse(a.recordDateTime))                                
+        //     return sortByDate.reverse();
+        // },        
+        // getWeightChartValues: function(){
+        //     let values = this.getWeightChartValuesSorted.map(a => [this.formatDateTime(a.recordDateTime), parseFloat(a.weight)]);
+        //     values.unshift(["DateTime", "Weight"]);
+        //     return values;
+        // },
+        // getPulseChartValuesSorted: function(){
+        //     let sortByDate = this.userPulseStats
+        //                         .slice() // Make copy of original array (avoids changing original array when sorting)
+        //                         .sort((a, b) => Date.parse(b.recordDateTime) - Date.parse(a.recordDateTime))
+        //     return sortByDate.reverse();
+        // },        
+        // getPulseChartValues: function(){
+        //     let values = this.getPulseChartValuesSorted.map(a => [this.formatDateTime(a.recordDateTime), parseInt(a.pulse)]);
+        //     values.unshift(["DateTime", "Pulse"]);
+        //     return values;
+        // },
+        // getBpChartValuesSorted: function(){
+        //     let sortByDate = this.userBpStats
+        //                         .slice() // Make copy of original array (avoids changing original array when sorting)
+        //                         .sort((a, b) => Date.parse(b.recordDateTime) - Date.parse(a.recordDateTime));
+        //     return sortByDate.reverse();
+        // },           
+        // getBpChartValues: function(){
+        //     let values = this.getBpChartValuesSorted.map( a => [this.formatDateTime(a.recordDateTime), a.sys, a.dia]);
+        //     values.unshift(["DateTime", "Sys", "Dia"]);
+        //     return values;
+        // }        
     },
     methods: {
         getUserData: function(){
@@ -192,34 +195,36 @@ export default {
             })                
         },
         formatDate(dateToFormat){
-            return new Date(dateToFormat).toLocaleDateString(); 
+            return commonService.formatDate(dateToFormat);
         },
-        formatDateTime(dateToFormat){
-            let thisDate = new Date(dateToFormat);
-            return thisDate.toLocaleString();  
-        },        
-        getLastRecordedDateDays(lastRecordedDate){
-            let lastDate = new Date(lastRecordedDate.recordDateTime); 
-            let today = new Date(); 
+        // formatDateTime(dateToFormat){
+        //     let thisDate = new Date(dateToFormat);
+        //     return thisDate.toLocaleString();  
+        // },        
+        // getLastRecordedDateDays(lastRecordedDate){
+        //     let lastDate = new Date(lastRecordedDate.recordDateTime); 
+        //     let today = new Date(); 
             
-            // To calculate the time difference of two dates 
-            var Difference_In_Time = today.getTime() - lastDate.getTime(); 
+        //     // To calculate the time difference of two dates 
+        //     var Difference_In_Time = today.getTime() - lastDate.getTime(); 
             
-            // To calculate the no. of days between two dates 
-            let diff = Difference_In_Time / (1000 * 3600 * 24);
+        //     // To calculate the no. of days between two dates 
+        //     let diff = Difference_In_Time / (1000 * 3600 * 24);
 
-            if(diff < 1){
-                return "Last recorded less than a day";
-            } 
-            else if (diff < 2)
-            {
-                return "Last recorded a day ago";
-            } 
-            return `${parseInt(diff)} days ago`;
-        }
+        //     if(diff < 1){
+        //         return "Last recorded less than a day";
+        //     } 
+        //     else if (diff < 2)
+        //     {
+        //         return "Last recorded a day ago";
+        //     } 
+        //     return `${parseInt(diff)} days ago`;
+        // }
     },
     components: {
-        GChart
+        WeightChart,
+        PulseChart,
+        BloodPressureChart
     },    
     created() {
         this.getUserData();
